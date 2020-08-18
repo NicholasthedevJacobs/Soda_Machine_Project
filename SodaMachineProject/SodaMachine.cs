0190f1dd038b8da075d5f97365a1d021ae3f6089
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-18 13:07:58 -0500

    Added the inventory items of the soda type to the constructor of the soda machine class

--- SodaMachineProject/SodaMachine.cs
+++ SodaMachineProject/SodaMachine.cs
@@ -21,26 +21,51 @@ namespace SodaMachineProject
             register = new List<Coin>();
             inventory = new List<Can>();
 
-            while (register.Count < 20)
+            if (register.Count < 100)
             {
-                Quarter quarter = new Quarter();
-                register.Add(quarter);
+                while (register.Count < 20)
+                {
+                    Quarter quarter = new Quarter();
+                    register.Add(quarter);
+                }
+                while (register.Count >= 20 && register.Count < 30)
+                {
+                    Dime dime = new Dime();
+                    register.Add(dime);
+                }
+                while (register.Count >= 30 && register.Count < 50)
+                {
+                    Nickle nickle = new Nickle();
+                    register.Add(nickle);
+                }
+                while (register.Count >= 50 && register.Count < 100)
+                {
+                    Penny penny = new Penny();
+                    register.Add(penny);
+                }
             }
-            while (register.Count >= 20 && register.Count < 30)
+            if (inventory.Count < 50)
             {
-                Dime dime = new Dime();
-                register.Add(dime);
+                while (inventory.Count < 15)
+                {
+                    Cola cola = new Cola();
+                    inventory.Add(cola);
+                }
+                while (inventory.Count >= 15 && inventory.Count < 30)
+                {
+                    OrangeSoda orangeSoda = new OrangeSoda();
+                    inventory.Add(orangeSoda);
+                }
+                while (inventory.Count >= 30 && inventory.Count < 50)
+                {
+                    RootBeer rootBeer = new RootBeer();
+                    inventory.Add(rootBeer);
+                }
             }
-            while (register.Count >= 30 && register.Count < 50)
-            {
-                Nickle nickle = new Nickle();
-                register.Add(nickle);
-            }
-            while (register.Count >= 50 && register.Count < 100)
-            {
-                Penny penny = new Penny();
-                register.Add(penny);
-            }
+
+
+           
+            
 
 
 
